4f5b12c5e5630263985c9cb769a4ec8c72da56dd
gsanson <glenn.sanson@gmail.com>
2016-06-04 06:35:00 +0200

    loader

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,21 @@
 // Our code!!!
+
+var ISC = {};
+
+
+ISC.launcher = function() {
+    ISC.game = new Phaser.Game(1536, 896, Phaser.AUTO, '', { preload: ISC.preload, create: ISC.create });
+}
+
+ISC.preload = function() {
+
+}
+
+
+ISC.create = function() {
+    
+}
+
+
+
+document.addEventListener("DOMContentLoaded", ISC.launcher, false);
