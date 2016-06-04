99298f73d6cc79a15b9208a9e7f4014efab02ab4
Wilka S. Jimenez <Wilka S. Jimenez>
2016-06-04 17:13:34 +0200

    [New] "fireRate" variable to towers

--- src/parameters.js
+++ src/parameters.js
@@ -3,18 +3,22 @@ var parameters = {
         tower_a0: {
             damage: 1,
             distance: 64 * 64
+            fireRate: 6
         },
         tower_a1: {
             damage: 2,
             distance: 64 * 64 * 4
+            fireRate: 3
         },
         tower_b0: {
             damage: 3,
             distance: 64 * 64 * 9
+            fireRate: 2
         },
         tower_b1: {
             damage: 4,
             distance: 64 * 64 * 16
+            fireRate: 1
         }
     },
 
