787549f898b079762573164d8a88699ac9ede4fa
gsanson <glenn.sanson@gmail.com>
2016-06-04 17:39:22 +0200

    Parametres ennemi

--- src/parameters.js
+++ src/parameters.js
@@ -33,15 +33,18 @@ var parameters = {
     enemies: {
         enemy_a0: {
             speed: 3,
-            energy: 50
+            energy: 50,
+            reward: 5
         },
         enemy_a1: {
             speed: 2,
-            energy: 200
+            energy: 200,
+            reward: 10
         },
         enemy_a2: {
             speed: 5,
-            energy: 30
+            energy: 30,
+            reward: 2
         }
     }
 };
