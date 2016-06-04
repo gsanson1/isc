d3d8992dbaf5adf5da9f7545b17d307dc25f8a8c
gsanson <glenn.sanson@gmail.com>
2016-06-04 17:37:41 +0200

    Parametres tour

--- src/parameters.js
+++ src/parameters.js
@@ -3,22 +3,30 @@ var parameters = {
         tower_a0: {
             damage: 1,
             distance: 64 * 64,
-            fireRate: 6
+            fireRate: 6,
+            cost: 50,
+            recycle: 30
         },
         tower_a1: {
             damage: 1,
             distance: 64 * 64 * 4,
-            fireRate: 3
+            fireRate: 3,
+            cost: 75,
+            recycle: 40
         },
         tower_b0: {
             damage: 2,
             distance: 64 * 64 * 9,
-            fireRate: 2
+            fireRate: 2,
+            cost: 75,
+            recycle: 40
         },
         tower_b1: {
             damage: 2,
             distance: 64 * 64 * 16,
-            fireRate: 1
+            fireRate: 1,
+            cost: 100,
+            recycle: 50
         }
     },
 
