23f986410d7b6d9778496811cb646e2754e8df5d
Maxime AILLOUD <maxime.ailloud@gmail.com>
2016-06-04 11:38:40 +0200

    Remove useless stuff

--- src/Game.js
+++ src/Game.js
@@ -22,7 +22,7 @@ ISC.Game = function (game) {
 
     // You can use any of these from any function within this State.
     // But do consider them as being 'reserved words', i.e. don't create a property for your own game called "world" or you'll over-write the world reference.
-    this.bg;
+    this.sea;
 };
 
 ISC.Game.prototype = {
@@ -30,18 +30,6 @@ ISC.Game.prototype = {
     create: function () {
 
         this.sea = this.add.sprite(0, 0, 'sea');
-        // this.map = this.add.tileSprite(0, 0, this.game.width, this.game.height, 'reef');
-
-        // this.map = game.add.tilemap();
-        //
-        // //  Add a Tileset image to the map
-        // this.map.addTilesetImage('reef');
-        //
-        // //  Creates a new blank layer and sets the map dimensions.
-        // //  In this case the map is 40x30 tiles in size and the tiles are 32x32 pixels in size.
-        // this.map = map.create('level1', 40, 30, 32, 32);
-        // this.map.scrollFactorX = 0.5;
-        // this.map.scrollFactorY = 0.5;
 
     },
 
@@ -64,9 +52,6 @@ ISC.Game.prototype = {
 
         // If the game container is resized this function will be called automatically.
         // You can use it to align sprites that should be fixed in place and other responsive display things.
-
-        this.sea.width = width;
-        this.sea.height = height;
 
     }
 
