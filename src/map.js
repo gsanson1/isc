4db2b20d2059b27dc4f5cc94ba490006e477bf2d
gsanson <glenn.sanson@gmail.com>
2016-06-05 16:25:06 +0200

    Protection plage

--- src/map.js
+++ src/map.js
@@ -45,6 +45,14 @@ Map.prototype = {
             this.compute(newMap, this.startX, this.startY);
             can = newMap[0][0] != 0;
         }
+        
+        if (_x == this.startX - 1 && _y == this.startY
+            || _x == this.startX && _y == this.startY - 1
+            || _x == this.startX && _y == this.startY + 1
+            || _x == this.startX - 1 && _y == this.startY - 1
+            || _x == this.startX - 1 && _y == this.startY + 1) {
+            can = false;
+        }
 
         return can;
     },
