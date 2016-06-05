74af94e0aa2fa3daf5e8840b1ca13dc12a050a2f
gsanson <glenn.sanson@gmail.com>
2016-06-05 15:34:19 +0200

    Ajout game over

--- src/endPage.js
+++ src/endPage.js
@@ -8,6 +8,7 @@ ISC.EndPage.prototype = {
 
     create: function () {
         this.splash = this.game.add.sprite(0, 0, 'endscr');
+        this.gameover = this.game.add.sprite(260, 483, 'gameover');
     },
     ActionOnClick: function () {
 
