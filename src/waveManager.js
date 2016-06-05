c2f35ad2bd075a62bd7071ff2985bc527a76aaf1
gsanson <glenn.sanson@gmail.com>
2016-06-05 13:47:48 +0200

    Ajout du ratio de lama basique

--- src/waveManager.js
+++ src/waveManager.js
@@ -12,8 +12,17 @@ WaveManager.prototype = {
         var randVal = this.countWave >= parameters.waves.waveGuests ? 4 : 2;
 
         var enemy = null;
+        var enemyNum;
         for (var i = 0; i < this.units; i++) {
-            enemy = new Enemy(this.gameScreen.game, this.gameScreen.map, -63, Tools.rand(704, 64), 'a' + Tools.rand(randVal));
+            if (Math.random() < parameters.waves.ratioBasic) {
+                enemyNum = 0; // Basic
+            } else if (randVal == 2) {
+                enemyNum = 1;
+            } else {
+                enemyNum = Tools.rand(randVal, 1);
+            }
+
+            enemy = new Enemy(this.gameScreen.game, this.gameScreen.map, -63, Tools.rand(704, 64), 'a' + enemyNum);
             enemy.energy *= this.strength;
             enemy.currentEnergy = enemy.energy;
             this.gameScreen.enemies.push(enemy);
